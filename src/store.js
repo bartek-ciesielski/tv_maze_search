f9869b40b49cbc361b7d4e9ab6dfefa5f9c53441
Bartek Ciesielski <bartlomiej-ciesielski@wp.pl>
2021-03-05 18:59:39 +0100

    Move close icon
    On mobiles it was hard to use it

--- src/store.js
+++ src/store.js
@@ -7,9 +7,9 @@ export default function configureStore(initialState) {
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      applyMiddleware(thunk)
+      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      //   window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
 }
